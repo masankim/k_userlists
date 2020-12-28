e9a60ce604c312435f4003ed3b8e10c865edf106
GaryKim <moduedu@gmail.com>
2020-12-28 22:07:16 +0900

    index.ejs step1

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const mysql  = require('mysql2')
+
+const db = mysql.createConnection({
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'1234',
+    database:'o2'
+})
 
 router.get('/data' ,function(request, response){
     console.log(request)
@@ -7,4 +16,17 @@ router.get('/data' ,function(request, response){
 })
 
 
+router.get('/topic', function(req, res){
+    let sql = 'SELECT * FROM topic'
+    db.query(sql, function(err,result){
+        if(err){
+            console.log(err)
+            res.status(500).send(err)
+        }else {
+            console.log(result)
+            res.render('index', {topics:result})
+        }
+    })
+    
+})
 module.exports = router;
